1f6a990bcfd4a8914cfe79514983c39b01eab5d1
NotSamarth <samarthvaishnav33@gmail.com>
2024-07-27 21:16:03 +0530

    find duplicate added

---
+++ findDuplicate.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> dup;
+        if (nums.size() <= 1) return dup;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(nums[abs(nums[i])-1] < 0){
+                dup.push_back(abs(nums[i]));
+            }
+            nums[abs(nums[i])-1] *= -1;
+        }
+        return dup;
+    }
+};
